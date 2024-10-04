07ebef3a4b432220c76622158085cec865cafdb3
praveen katewa <praveenkatewa.45@gmail.com>
2024-10-05 01:34:23 +0530

    first commit

---
+++ start/src/App.jsx
@@ -0,0 +1,34 @@
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import About from './components/About'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <><Navbar /><Home /></>
+    },
+    {
+      path: "/login",
+      element: <><Navbar /><Login /></>
+    },
+    {
+      path: "/about",
+      element: <><Navbar /><About /></>
+    },
+   
+  ])
+  return (
+    <>
+      
+      <RouterProvider router={router} />
+
+    </>
+  )
+}
+
+export default App
