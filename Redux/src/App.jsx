5a6e0530c598f5e8767d5130afda09c6ae4165f1
praveen katewa <praveenkatewa.45@gmail.com>
2024-12-26 01:42:36 +0530

    new files

---
+++ Redux/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+
+// import './App.css'
+import './input.css'
+import './index.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1> this is redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+     
+    </>
+  )
+}
+
+export default App
