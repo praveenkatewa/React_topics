cccefa838a0a56635a27dbd6c58ad7f5f3f435d6
praveen katewa <praveenkatewa.45@gmail.com>
2025-01-04 22:32:29 +0530

    new file

--- Spotify_new/src/App.jsx
+++ Spotify_new/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
+import Display from './components/Display'
+import {PlayerContext} from './context/PlayerContext'
+import { useContext } from 'react'
 
 const App = () => {
+
+  const {audioRef,track}=useContext(PlayerContext);
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex' >
       <Sidebar/>
+      <Display/>
+
        </div>
        <Player/>
+       <audio ref={audioRef} src={track.file}preload='auto'></audio>
     </div>
   )
 }
--- Spotify_new/src/components/Player.jsx
+++ Spotify_new/src/components/Player.jsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import {  assets,songsData } from '../assets/assets'
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { PlayerContext } from '../context/PlayerContext'
 
 const Player = () => {
+
+
+  const{ track,seekBar,seekBg,PlayStatus,play,pause,time,previous,next,seekSong}=useContext(PlayerContext)
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4 '>
     <div className='hidden lg:flex items-center gsp-4'>
-    <img className='w-12' src={songsData[0].image} at=" "/>
+    <img className='w-12' src={track.image} at=" "/>
     <div>
-       <p>{songsData[0].name}</p>
-       <p>{songsData[0].desc.slice(0,12)}</p>
+       <p>{track.name}</p>
+       <p>{track.desc.slice(0,12)}</p>
 
     </div>
     </div>
     <div className='flex flex-col items-center gap-1 m-auto'>
     <div className='flex gap-4'>
     <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt=""/>
-    <img className='w-4 cursor-pointer' src={assets.prev_icon} alt=""/>
-    <img className='w-4 cursor-pointer' src={assets.play_icon} alt=""/>
-    <img className='w-4 cursor-pointer' src={assets.next_icon} alt=""/>
+    <img  onClick={previous} className='w-4 cursor-pointer' src={assets.prev_icon} alt=""/>
+    {PlayStatus
+     ?<img  onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt=""/>
+    : <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt=""/>
+    }
+   
+   
+    <img  onClick={next} className='w-4 cursor-pointer' src={assets.next_icon} alt=""/>
     <img className='w-4 cursor-pointer' src={assets.loop_icon} alt=""/> 
      </div>
 
     <div className='flex items-center gap-5'>
-    <P>1:06</P>
-    <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-    <hr className='h-1 border-none w-0 bg-green-800 rounded-full'/>
+    <p>{time.currentTime.minute}:{time.currentTime.second}</p>
+    <div ref={seekBg} onClick={seekSong} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+    <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full'/>
+    </div>
+    <p>{time.totalTime.minute}:{time.totalTime.second}</p>
+    </div>
+    </div>
+
+
+    <div className='hidden lg:flex items-center gap=2 opacity=-75'>
+    <img  className='w-4'src={assets.play_icon} />
+    <img  className='w-4'src={assets.mic_icon} />
+    <img  className='w-4'src={assets.queue_icon} />
+    <img  className='w-4'src={assets.speaker_icon} />
+    <img  className='w-4'src={assets.volume_icon} />
+    <div className='w-20 bg-slate-50 h-1 rounded'>
 
     </div>
-    {/* <p>3:20</p> */}
+    <img  className='w-4'src={assets.mini_player_icon} />
+    <img  className='w-4'src={assets.zoom_icon} />
 
-
-    </div>
 
     </div>
     </div>
